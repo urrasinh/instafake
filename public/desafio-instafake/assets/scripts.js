a22de033475872400e4880ef6be21e91a939d6ed
urrasinh <46355836+urrasinh@users.noreply.github.com>
2022-04-05 18:18:38 -0400

    Update scripts.js

--- public/desafio-instafake/assets/scripts.js
+++ public/desafio-instafake/assets/scripts.js
@@ -1,55 +1,87 @@
-$('#js-form').submit(async (event) => {
-    event.preventDefault()
-    const email = document.getElementById('js-input-email').value
-    const password = document.getElementById('js-input-password').value
-    const JWT = await postData(email, password)
-    const posts = await getFotos(JWT)
-    //fillCard(posts, 'js-table-posts')
-    toggleFormAndTable('js-form-wrapper', 'js-table-wrapper')
-    //console.log(posts)
-})
+(() => {  // inicio IIFE
+    const formularioSelector = document.querySelector('#js-form')
+    const feedUnicoSelector = document.querySelector('#mostrar-feed')
+    const contenedorImagenesSelector = document.querySelector('#contenedor-imagenes')
+    const logoutSelector = document.querySelector('#cerrar-sesion')
+    const masImagenesSelector = document.querySelector('#cargar-mas-imagenes')
+    let imagenFeed = ''
+    let pageNum = 1
 
-const postData = async (email, password) => {
-    try {
-        const response = await fetch('http://localhost:3000/api/login',
-            {
-                method: 'POST',
-                body: JSON.stringify({ email: email, password: password })
-            })
-        const { token } = await response.json()
-        localStorage.setItem('jwt-token', token) // persistir jws
-        return token
-    } catch (err) {
-        console.error(`Error: ${err}`)
-    }
-}
+    formularioSelector.addEventListener('submit', async (event) => { 
+        event.preventDefault()
+        const email = document.querySelector('#correo-electronico').value
+        const password = document.querySelector('#contrasena').value
+        await postData(email, password)
+        getFoto(pageNum)
+        document.querySelector('#correo-electronico').value = ''
+        document.querySelector('#contrasena').value = ''
+    })
 
-const getFotos = async (jwt) => {
-    try {
-        const response = await fetch('http://localhost:3000/api/photos',
-            {
-                method: 'GET',
-                headers: {
-                    Authorization: `Bearer ${jwt}`
-                }
-            })
-        const { data } = await response.json()
-        // return data
-        if (data) {
-            fillCard(data, 'js-table-posts')
-            toggleFormAndTable('js-form-wrapper', 'js-table-wrapper')
+    const postData = async (email, password) => {
+        try {
+            const response = await fetch('http://localhost:3000/api/login',
+                {
+                    method: 'POST',
+                    body: JSON.stringify({ email, password })
+                })
+            const { token } = await response.json()
+            localStorage.setItem('jwt-token', token)            
+            return token
+        } catch (error) {
+            console.log(`Error: ${error}`)
         }
+    }
 
-    } catch (err) {
-        console.error(`Error: ${err}`)
+    const getFoto = async (numeroPagina) => { 
+        const jwt = localStorage.getItem('jwt-token')
+        try {
+            const response = await fetch(`http://localhost:3000/api/photos?page=${numeroPagina}`,
+                {
+                    method: 'GET',
+                    headers: {
+                        Authorization: `Bearer ${jwt}`
+                    }
+                })
+            const { data } = await response.json()
+            if (data) {
+                contenedorImagenesSelector.setAttribute("style", "display: block")
+                formularioSelector.setAttribute("style", "display: none")
+                data.forEach((datos) => {
+                    imagenFeed += `
+                    <div class="card" id="tarjeta">
+                        <img width="600" height="800" src="${datos.download_url}" class="card-img-top"/>
+                        <div class="card-footer">
+                            <span>${datos.author}</span>
+                        </div>
+                    </div>
+                    `
+                })
+                feedUnicoSelector.innerHTML = imagenFeed
+            }
+        } catch (error) {
+            console.log(error)
+        }
     }
-}
-const init = async () => {
-    const token = localStorage.getItem('jwt-token')
-    if (token) {
-        const posts = await getFotos(token)
-        fillCard(posts, 'js-table-posts')
-        toggleFormAndTable('js-form-wrapper', 'js-table-wrapper')
+    //  de logout que elimine el JWT 
+    logoutSelector.addEventListener('click', () => {
+        localStorage.removeItem('jwt-token')
+        localStorage.clear()
+        feedUnicoSelector.innerHTML = ''
+        contenedorImagenesSelector.setAttribute("style", "display: none")
+        formularioSelector.setAttribute("style", "display: block")
+    })     
+
+    masImagenesSelector.addEventListener('click', () => {
+        pageNum++
+        getFoto(pageNum)
+    }) 
+
+    const iniciar = () => {
+        const token = localStorage.getItem('jwt-token')
+        if (token) {
+            getFoto(pageNum)
+        }
     }
-}
-init()
+    iniciar() // Cargar el feed de fotos cuando exista el JWT
+    
+})() // fin IIFE
